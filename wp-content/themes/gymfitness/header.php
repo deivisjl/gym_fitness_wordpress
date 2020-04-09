e36c4adf8b84f955755167632c2973c8e7d7a596
deivisjl <deivisjl@gmail.com>
2020-04-08 19:28:31 -0600

    Agregando assets

---
+++ wp-content/themes/gymfitness/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+	
+<header class="site-header">
+	<div class="contenedor">
+		<div class="barra-navegacion">
+			<div class="logo">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="logo_sitio">
+			</div>
+		</div>
+	</div>
+</header>
--- wp-content/themes/gymfitness/page.php
+++ wp-content/themes/gymfitness/page.php
@@ -1,3 +1,5 @@
+<?php 	get_header(); ?>
+
 <?php while(have_posts()):the_post(); ?>
 	
 	<h1><?php the_title(); ?></h1>
@@ -9,3 +11,5 @@
 	Fecha: <?php the_date(); ?>
 
 <?php endwhile; ?>
+
+<?php 	get_footer(); ?>
