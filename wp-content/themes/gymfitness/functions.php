e5622e42e6734c97594231ee9dbdb25bdc2a1a53
deivisjl <deivisjl@gmail.com>
2020-04-09 08:10:06 -0600

    Agregando fuentes de google

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -14,8 +14,14 @@
 	//cargar scripts y hojas de estilos
 	function gymfitness_scripts_styles(){
 
+		//cargando normalize
+		wp_enqueue_style('normalize', get_template_directory_uri().'/css/normalize.css', array(), '8.0.1');
+
+		//fuentes de google
+		wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap',array(),'1.0.0');
+
 		//la hoja de estilos principal
-		wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
+		wp_enqueue_style('style', get_stylesheet_uri(), array('normalize','googleFont'), '1.0.0');
 	}
 
 	add_action('wp_enqueue_scripts','gymfitness_scripts_styles');
