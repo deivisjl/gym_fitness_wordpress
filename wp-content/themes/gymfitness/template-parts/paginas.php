01ace0da6af8cc0d902d5c4d526f4d1927ddd144
deivisjl <deivisjl@gmail.com>
2020-04-11 12:52:53 -0600

    Mostrando detalle de las clases

--- wp-content/themes/gymfitness/template-parts/paginas.php
+++ wp-content/themes/gymfitness/template-parts/paginas.php
@@ -10,6 +10,17 @@
 
 	?>
 
+	<?php 
+	  //Revisar si el custom post type es clases
+
+		if(get_post_type() === 'gymfitness_clases')
+		{
+				$hora_inicio = get_field('hora_inicio');
+				$hora_fin = get_field('hora_fin');
+				 ?>
+				 <p class="informacion-clase"><?php the_field('dias_clase'); ?> - <?php echo $hora_inicio . " a " . $hora_fin; ?></p>
+		<?php }?>
+
 	<?php the_content(); ?>
 
 	Escrito por: <?php the_author(); ?>
