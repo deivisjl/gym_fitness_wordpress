907784baf6f66b18b578417531cf3d09c7e635e7
deivisjl <deivisjl@gmail.com>
2020-04-11 16:42:36 -0600

    Mostrando las imagenes en el lightbox

--- wp-content/themes/gymfitness/page-galerias.php
+++ wp-content/themes/gymfitness/page-galerias.php
@@ -11,12 +11,29 @@
 			<?php 
 
 				//obtener la galeria de la pagina actual
-				$galeria = get_post_gallery(get_the_ID(),false) 
+				$galeria = get_post_gallery(get_the_ID(),false); 
 
 				$galeria_imagenes_ID = explode(',', $galeria['ids']);
 			?>
 			<ul class="galeria-imagenes">
-				
+				<?php  
+
+					$i = 1;
+
+					foreach($galeria_imagenes_ID as $id):
+
+						$size = ($i == 4 || $i == 6) ? 'portrait' : 'square';
+
+						$imagenThumb = wp_get_attachment_image_src($id, $size)[0];
+						$imagen = wp_get_attachment_image_src($id, 'full')[0];?>
+
+						<li>
+							<a href="<?php echo $imagen ?>" data-lightbox="galeria">
+								<img src="<?php echo $imagenThumb ?>" alt="imagen">
+							</a>
+						</li>						
+					<?php $i++; endforeach;
+				?>
 			</ul>
 
 		<?php  endwhile; ?>
