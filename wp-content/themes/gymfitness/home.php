0e34d6582e77ba26856b230c8e8fd467d98b3a55
deivisjl <deivisjl@gmail.com>
2020-04-11 18:21:37 -0600

    CSS para el blog

--- wp-content/themes/gymfitness/home.php
+++ wp-content/themes/gymfitness/home.php
@@ -13,8 +13,8 @@
 							<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
 								<?php echo get_the_author_meta('display_name'); ?>
 							</a>
-							<?php the_time(get_option('date_format')); ?>
 						</p>
+						<p class="meta"> <span>Fecha: </span><?php the_time(get_option('date_format')); ?></p>
 					</div>
 				</li>
 			<?php endwhile; ?>
