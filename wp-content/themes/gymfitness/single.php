13a9d3458d8f804bf71e03ed46a22f67b1c7516d
deivisjl <deivisjl@gmail.com>
2020-04-11 19:18:05 -0600

    Agregando widget al sidebar

--- wp-content/themes/gymfitness/single.php
+++ wp-content/themes/gymfitness/single.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
-<h1>Hola desde single.php</h1>
+<main class="contenedor pagina seccion con-sidebar">
+	<div class="contenido-principal">
+		<?php get_template_part('template-parts/paginas'); ?>
+	</div>
+	<?php get_sidebar(); ?>
+</main>
 
 <?php 	get_footer(); ?>
