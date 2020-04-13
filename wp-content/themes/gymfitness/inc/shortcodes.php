242d40ab2209bdd55ecc01e5501845a4ac970a25
deivisjl <deivisjl@gmail.com>
2020-04-13 07:42:12 -0600

    Ubicando el marker en el mapa

--- wp-content/themes/gymfitness/inc/shortcodes.php
+++ wp-content/themes/gymfitness/inc/shortcodes.php
@@ -8,7 +8,7 @@
 			<input type="hidden" id="lat" value="<?php echo $ubicacion['lat']; ?>">
 			<input type="hidden" id="lng" value="<?php echo $ubicacion['lng']; ?>">
 			<input type="hidden" id="direccion" value="<?php echo $ubicacion['address']; ?>">
-			<div id="mapa"></div>
+			<div id="map"></div>
 		</div>
 		<?php 
 	}
--- wp-content/themes/gymfitness/js/scripts.js
+++ wp-content/themes/gymfitness/js/scripts.js
@@ -5,13 +5,20 @@ jQuery(document).ready(function($){
 	});
 
 	//Codigo del mapa
-	var map = L.map('map').setView([51.505, -0.09], 13);
+	const lat = document.querySelector('#lat').value;
+	const lng = document.querySelector('#lng').value;
+	const direccion = document.querySelector('#direccion').value;
 
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+	if(lat && lng && direccion)
+	{
+		var map = L.map('map').setView([lat, lng], 13);
 
-	L.marker([51.5, -0.09]).addTo(map)
-	    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-	    .openPopup();
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		L.marker([lat, lng]).addTo(map)
+		    .bindPopup(direccion)
+		    .openPopup();
+	}
 });
